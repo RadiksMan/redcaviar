485b14016cf755a305f2e2d8d9de37c94a8ff739
Radion <radion91@gmail.com>
2015-06-12 12:23:48 +0200

    no message

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/slick.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/dima.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+	<script src="js/jquery-1.10.0.min.js" type="text/javascript"></script>
+
+	<script src="js/selectivizr-min.js" type="text/javascript"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/jquery.znice.css" media="all" />
+	<script src="js/jquery.validate.min.js" type="text/javascript"></script>
+	<script src="js/jquery.znice.validate.js" type="text/javascript"></script>
+	<script src="js/jquery.znice.js" type="text/javascript"></script>
+
+	<link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox.css" media="all" />
+	<script src="fancybox/jquery.fancybox.js" type="text/javascript"></script>
+
+    <script src="js/modernizr.js" type="text/javascript"></script>
+	<script src="js/slick.js" type="text/javascript"></script>
+	<script src="js/jquery.watermark.min.js" type="text/javascript"></script>
+
+
+<!-- jScrollPane -->
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+<!-- jScrollPane -->
+
+    <script src="js/dima.js" type="text/javascript"></script>
+	<script src="js/scr.js" type="text/javascript"></script>
+
+	<!--[if lt IE 9]>
+		<script>
+		document.createElement('header');
+		document.createElement('nav');
+		document.createElement('section');
+		document.createElement('article');
+		document.createElement('aside');
+		document.createElement('footer');
+		document.createElement('time');
+		</script>
+		<script src="js/pie.js" type="text/javascript"></script>
+	<![endif]-->
+
+	<!--[if lt IE 8]><script src="js/oldie/warning.js"></script><script>window.onload=function(){e("js/oldie/")}</script><![endif]-->
+</head>
+<body>
+	<?partial('zHiddenBlock');?>
+	<header class="header">
+		<div class="mbox">
+
+		</div>
+	</header>
